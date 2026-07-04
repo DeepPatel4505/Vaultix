e0829cb396ba0a82b54133a9c1b091855873a6fc
Deep Patel <deeppatel5501@gmail.com>
2026-07-04 14:44:07 +0530

    feat: update permit limit for authentication rate limiting to enhance security

--- FileShareAPI/Options/AuthRateLimitOptions.cs
+++ FileShareAPI/Options/AuthRateLimitOptions.cs
@@ -4,7 +4,7 @@ public class AuthRateLimitOptions
 {
     public const string Position = "AuthRateLimit";
 
-    public int PermitLimit { get; set; } = 5;
+    public int PermitLimit { get; set; } = 100;
     public int WindowSeconds { get; set; } = 60;
     public int QueueLimit { get; set; } = 0;
 }
