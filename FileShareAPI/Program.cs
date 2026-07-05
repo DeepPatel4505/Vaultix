a196c0bcce49d159937e1e397b349b2444b0b45c
Deep Patel <deeppatel5501@gmail.com>
2026-07-05 11:33:46 +0530

    feat: clear known IP networks and proxies in ForwardedHeadersOptions configuration refactor: remove localhost from allowed origins in production CORS settings

--- FileShareAPI/Program.cs
+++ FileShareAPI/Program.cs
@@ -57,6 +57,8 @@ builder.Services.Configure<ForwardedHeadersOptions>(options =>
     options.ForwardedHeaders =
         ForwardedHeaders.XForwardedFor |
         ForwardedHeaders.XForwardedProto;
+    options.KnownIPNetworks.Clear();
+    options.KnownProxies.Clear();
 });
 
 builder.Services.Configure<AuthCookieOptions>(
